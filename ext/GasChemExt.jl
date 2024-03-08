f435179a185c179791b52592436a8be9bc048d89
jialinl6 <jialinl6@illinois.edu>
2024-03-08 00:10:36 -0600

    Add extensions

---
+++ ext/GasChemExt.jl
@@ -0,0 +1,9 @@
+module GasChemExt
+
+Base.:(+)(w::Wetdeposition, b::SuperFast) = operator_compose(b, w)
+Base.:(+)(b::SuperFast, w::Wetdeposition) = w + b
+
+Base.:(+)(d::DrydepositionG, b::SuperFast) = operator_compose(b, d)
+Base.:(+)(b::SuperFast, d::DrydepositionG) = d + b
+
+end
